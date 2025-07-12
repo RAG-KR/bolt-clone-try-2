1141ab4a8ffdcc4768791c427959799351d3af81
Raghav_Kr <raghav.r@cimpress.com>
2025-07-12 19:02:03 +0530

    code veiw done now we have to generate the project files and the project

--- bolt_new_clone/components/ui/custom/CodeView.jsx
+++ bolt_new_clone/components/ui/custom/CodeView.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Lookup from "../../../data/Lookup";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
 
 function CodeView() {
+  const [activeTab, setActiveTab] = useState("code");
+  const [files, setFiles] = useState(Lookup?.DEFAULT_FILE)
   return (
-    <div>CodeView</div>
-  )
+    <div>
+      <div className="bg-[#181818] w-full p-2 border">
+        <div className="flex items-center flex-wrap shrink-0 p-1 bg-black w-[140px] gap-3 justify-center rounded-full">
+          <h2
+            onClick={() => setActiveTab("code")}
+            className={`text-sm cursor-pointer ${activeTab == "code" && "bg-blue-500/25 text-blue-500 p-1 px-2 rounded-full"}`}
+          >
+            Code
+          </h2>
+          <h2
+            onClick={() => setActiveTab("preview")}
+            className={`text-sm cursor-pointer ${activeTab == "preview" && "bg-blue-500/25 text-blue-500 p-1 px-2 rounded-full"}`}
+          >
+            Preview
+          </h2>
+        </div>
+      </div>
+      <SandpackProvider template="react" theme="dark"
+      files={files}
+      customSetup={
+        {
+          dependencies:{
+            ...Lookup.DEPENDANCY,
+          }
+        }
+      }
+      options={{
+        externalResources:['https://cdn.tailwindcss.com']
+      }}
+      >
+        <SandpackLayout>
+          {activeTab == "code" ? <>
+            <SandpackFileExplorer style={{ height: "80vh" }} />
+            <SandpackCodeEditor style={{height: "80vh",}}/>
+          </>
+          :
+          <>
+            <SandpackPreview style={{height: "80vh",}} showNavigator={true}/>
+          </>
+          }
+        </SandpackLayout>
+      </SandpackProvider>
+    </div>
+  );
 }
 
-export default CodeView
+export default CodeView;
